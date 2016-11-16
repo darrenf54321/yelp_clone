e09dda02b0480356bfab110dfe08c2ae3278ed89
david anderson <d.a.m.anderson@gmail.com>
2016-11-16 17:18:56 +0000

    fb login still not working, problems with env variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -35,7 +35,9 @@ Devise.setup do |config|
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [:email]
-
+  config.omniauth :facebook, "FACEBOOK_APP_ID", "FACEBOOK_APP_SECRET",
+                  callback_url: "http://localhost:3000/users/auth/facebook/callback",
+                  scope: 'email', display: 'popup'
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
   # find_for_authentication method and considered in your model lookup. For instance,
